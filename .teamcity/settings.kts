4ef431e1d7cadad3e09a384eea80b050d2d1007f
Warner <warner.hooh@gmail.com>
2019-04-29 18:27:42 +0800

    add integration test

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -18,7 +18,8 @@ To debug settings scripts in command-line, run the
 
 command and attach your debugger to the port 8000.
 
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (Vie
+w
 -> Tool Windows -> Maven Projects), find the generate task node
 (Plugins -> teamcity-configs -> teamcity-configs:generate), the
 'Debug' option is available in the context menu for the task.
